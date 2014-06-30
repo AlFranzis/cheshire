656e70cb279e989f996e5fdaa711c23541f57747
AXJRD <alexander.hoermandinger@agfa.com>
2014-06-30 14:08:31 +0200

    Added example for OSGi service

--- cheshire.osgi/src/main/java/al/franzis/cheshire/osgi/OSGiModuleContext.java
+++ cheshire.osgi/src/main/java/al/franzis/cheshire/osgi/OSGiModuleContext.java
@@ -1,11 +1,13 @@
 package al.franzis.cheshire.osgi;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.osgi.framework.BundleContext;
 
 import al.franzis.cheshire.IModule;
 import al.franzis.cheshire.IModuleContext;
+import al.franzis.cheshire.service.IServiceReference;
 
 public class OSGiModuleContext implements IModuleContext {
 	private final BundleContext bundleContext;
@@ -26,6 +28,29 @@ public class OSGiModuleContext implements IModuleContext {
 
 	public List<IModule> getModules() {
 		return null;
+	}
+
+	@Override
+	public <S> void registerService(Class<S> serviceClass, S service) {
+	}
+
+	@Override
+	public <S> IServiceReference<S> getServiceReference(Class<S> clazz) {
+		return null;
+	}
+
+	@Override
+	public <S> Collection<IServiceReference<S>> getServiceReferences(Class<S> clazz) {
+		return null;
+	}
+
+	@Override
+	public <S> S getService(IServiceReference<S> serviceReference) {
+		return null;
+	}
+
+	@Override
+	public void ungetService(IServiceReference<? extends Object> serviceReference) {
 	}
 	
 }
