606e74e6b2fefd1a14526ef75ea410c3e00f970a
AXJRD <alexander.hoermandinger@agfa.com>
2014-07-18 16:09:39 +0200

    Further improvements on CDI native lib loading

--- cheshire.test.cdi.native/src/main/java/cheshire/test/cdi/helloworld/HelloWorldStarter.java
+++ cheshire.test.cdi.native/src/main/java/cheshire/test/cdi/helloworld/HelloWorldStarter.java
@@ -2,22 +2,18 @@ package cheshire.test.cdi.helloworld;
 
 import org.jboss.weld.environment.se.StartMain;
 
-import al.franzis.cheshire.IModuleContext;
-import cheshire.test.cdi.activator.ModuleActivatorExample;
 import cheshire.test.cdi.nat.JNIHelloWorld;
 
 public class HelloWorldStarter {
 	
 	public static void main(String[] args) {
 		StartMain.main(args);
-		usePluginService();
+		useJNI();
 	}
-	
 
-	private static void usePluginService() {
+	private static void useJNI() {
 		JNIHelloWorld hw = new JNIHelloWorld();
 		hw.sayHello();
-		
 	}
 	
 }
