7bc2b4f6cbae76f3703be04c7e3ff6d081d40cda
AXJRD <alexander.hoermandinger@agfa.com>
2014-09-02 13:25:01 +0200

    Code cleanup

--- cheshire.test.cdi/src/main/java/cheshire/test/cdi/helloworld/HelloWorldPrinter.java
+++ cheshire.test.cdi/src/main/java/cheshire/test/cdi/helloworld/HelloWorldPrinter.java
@@ -9,20 +9,11 @@ import javax.inject.Inject;
 import org.jboss.weld.environment.se.bindings.Parameters;
 import org.jboss.weld.environment.se.events.ContainerInitialized;
 
-import al.franzis.cheshire.IModuleContext;
-import al.franzis.cheshire.service.IServiceReference;
-import cheshire.test.cdi.activator.ModuleActivatorExample;
-import cheshire.test.cdi.service.IPlugin;
-import cheshire.test.cdi.service.IPluginManager;
-
 
 public class HelloWorldPrinter {
 	
 	@Inject
 	private HelloWorldPojo pojo;
-	
-//	@Inject
-//	private IPluginManager pluginManager;
 	
 	public void printHello(@Observes ContainerInitialized event,
 			@Parameters List<String> parameters) {
